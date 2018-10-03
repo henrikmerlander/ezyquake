1b5841528d08513256a498352e08057ccf348db2
Henrik Merlander <henrik.merlander@gmail.com>
2018-10-03 19:47:02 +0200

    refactor(): extract map component

--- src/views/RandomMap.vue
+++ src/views/RandomMap.vue
@@ -15,30 +15,19 @@
       <p>Some maps are only available in the <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=539421606">Quake Live Extras Pack</a></p>
     </v-flex>
     <v-flex xs12 sm6>
-      <v-dialog-transition>
-        <v-card class="random-map" v-if="randomMap" max-width="500">
-          <v-img class="white--text" :height="500" :src="'http://epsy46.free.fr/qlmaps/levelshots/' + randomMap.key + '.jpg'">
-            <v-container fill-height fluid>
-              <v-layout fill-height>
-                <v-flex xs12 align-end flexbox>
-                  <span class="headline">{{ randomMap.name }}</span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-img>
-          <v-card-title>
-            <v-chip label small disabled outline v-for="tag in randomMap.tags" :key="tag">{{ tag }}</v-chip>
-          </v-card-title>
-        </v-card>
-      </v-dialog-transition>
+      <Map :map="randomMap" />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import maps from '@/data/maps'
+import Map from '@/components/Map'
 
 export default {
+  components: {
+    Map,
+  },
   data() {
     return {
       gameType: 'all',
