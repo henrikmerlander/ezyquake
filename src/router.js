e9a9f4c8d2e4a53f90eb0aa870cb97a8b2d9ad0a
Henrik Merlander <henrik.merlander@gmail.com>
2018-10-03 19:53:51 +0200

    style(app): format files

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import RandomMap from '@/views/RandomMap.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import RandomMap from '@/views/RandomMap.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   mode: 'history',
@@ -18,4 +18,4 @@ export default new Router({
       component: RandomMap,
     },
   ],
-});
+})
