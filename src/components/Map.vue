40e4099871a0e1b860902daeb9c2b44f625122da
Henrik Merlander <henrik.merlander@gmail.com>
2018-10-07 16:05:53 +0200

    style(map): format template

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,13 +1,13 @@
 <template>
   <v-card max-width="500">
     <v-img class="white--text" :height="400" :src="'http://epsy46.free.fr/qlmaps/levelshots/' + map.key + '.jpg'">
-    <v-container>
-      <v-layout>
-        <v-flex>
-          <span class="headline">{{ map.name }}</span>
-        </v-flex>
-      </v-layout>
-    </v-container>
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <span class="headline">{{ map.name }}</span>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-img>
     <v-card-title>
       <v-chip label small disabled outline v-for="tag in map.tags" :key="tag">{{ tag }}</v-chip>
